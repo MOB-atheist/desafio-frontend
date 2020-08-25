7317360cf03d73286ad7a5acd01171a54f7c6268
pablo <pabskne@gmail.com>
2020-08-25 12:22:58 -0300

    Add build

---
+++ docs/precache-manifest.998042bae095487ea2ec22e740ab3953.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce2bd0dd43f81a056f24880ba143b5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ab0b99df7959cff92e0",
+    "url": "/static/css/main.3a463751.chunk.css"
+  },
+  {
+    "revision": "10d95d1077a61f53f6d3",
+    "url": "/static/js/2.6803a44e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6803a44e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ab0b99df7959cff92e0",
+    "url": "/static/js/main.653cb797.chunk.js"
+  },
+  {
+    "revision": "f294869ae1697dfe421d",
+    "url": "/static/js/runtime-main.7ecb0647.js"
+  },
+  {
+    "revision": "dcc05c8f08630308fd6d7bb1ca10179b",
+    "url": "/static/media/search.dcc05c8f.svg"
+  }
+]);
