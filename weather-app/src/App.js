d79c8416650948456c0cad6b43daca7379e2bc47
pablo <pabskne@gmail.com>
2020-08-25 09:57:01 -0300

    call created modules

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './app.css'
+import Search from './modules/Search';
+import WeatherWidget from './modules/WeatherWidget';
+import WeatherList from './modules/WeatherList';
 function App() {
 	return (
 		<div className="container">
 			<h1 className="container-title" >
 				Previsão do tempo
 			</h1>
+			<Search />
+			<WeatherWidget />
+			<WeatherList />
 		</div>
 	);
 }
