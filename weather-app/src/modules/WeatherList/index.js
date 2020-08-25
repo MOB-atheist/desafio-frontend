62dfca3aa330752e9900d0b7b86e6982a9eb1a0f
pablo <pabskne@gmail.com>
2020-08-25 12:12:00 -0300

    Added tables for styling process

--- weather-app/src/modules/WeatherList/index.js
+++ weather-app/src/modules/WeatherList/index.js
@@ -3,6 +3,82 @@ import './styles.css'
 
 export default class WeatherList extends Component {
     render() {
-        return <div></div>
+        return(
+            <div className="WeatherList">
+                <h2>Capitais</h2>
+                <div className="side-tables">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Min</th>
+                                <th>Max</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>18°</td>
+                                <td>27°</td>
+                                <td>RIo de Janeiro</td>
+                            </tr>
+                            <tr>
+                                <td>18°</td>
+                                <td>27°</td>
+                                <td>RIo de Janeiro</td>
+                            </tr>
+                            <tr>
+                                <td>18°</td>
+                                <td>27°</td>
+                                <td>RIo de Janeiro</td>
+                            </tr>
+                            <tr>
+                                <td>18°</td>
+                                <td>27°</td>
+                                <td>RIo de Janeiro</td>
+                            </tr>
+                            <tr>
+                                <td>18°</td>
+                                <td>27°</td>
+                                <td>RIo de Janeiro</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Min</th>
+                                <th>Max</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>18°</td>
+                                <td>27°</td>
+                                <td>RIo de Janeiro</td>
+                            </tr>
+                            <tr>
+                                <td>18°</td>
+                                <td>27°</td>
+                                <td>RIo de Janeiro</td>
+                            </tr>
+                            <tr>
+                                <td>18°</td>
+                                <td>27°</td>
+                                <td>RIo de Janeiro</td>
+                            </tr>
+                            <tr>
+                                <td>18°</td>
+                                <td>27°</td>
+                                <td>RIo de Janeiro</td>
+                            </tr>
+                            <tr>
+                                <td>18°</td>
+                                <td>27°</td>
+                                <td>RIo de Janeiro</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        )
     }
 }
